fbfaf7ddafe43d8ce1c493312e0144989b0cc919
Kim Daul <elre519@naver.com>
2025-12-27 18:37:42 +0900

    프론트 및 배포 관련 수정

--- frontend/src/component/userService/Login.tsx
+++ frontend/src/component/userService/Login.tsx
@@ -29,7 +29,7 @@ export default function Login() {
       localStorage.setItem("refreshToken", response.refreshToken);
       localStorage.setItem("userSignId", response.userSignId);
       localStorage.setItem("role", response.role);
-      localStorage.setItem("profileImg", response.profileImg);
+      localStorage.setItem("profileImg", response.profileImg ?? "");
 
       // 4. 로그인 성공 후 페이지 이동 (예: 메인 페이지)
       router.push("/");
