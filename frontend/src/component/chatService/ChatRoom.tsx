4058b4bd9bf480ccd045e18e31f778c9632dcf1b
Kim Daul <elre519@naver.com>
2026-01-04 22:46:36 +0900

    chat

--- frontend/src/component/chatService/ChatRoom.tsx
+++ frontend/src/component/chatService/ChatRoom.tsx
@@ -112,7 +112,7 @@ export default function ChatRoom({
 
   // ⭐ roomId를 꼭 전달해야 구독이 됩니다!
   const { isConnected, sendMessage } = useWebSocket({
-    url: 'wss://localhost:1005/ws-chat',
+    url: 'ws://111111/ws-chat',
     roomId: roomId,
     onMessage: handleNewMessage,
   });
